9716775441954e85b7c2e5fa88a3f0cb5c74fbd3
Brendan LaMarche <drac@dracsoft.com>
2013-11-03 00:47:57 -0400

    csharp version

--- cs-scan.cs
+++ cs-scan.cs
@@ -1,15 +1,80 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Collections;
+using System.Diagnostics;
+using System.Net.Sockets;
 
 namespace cs_scan
 {
+    class ScanSeg
+    {
+        public int starti;
+        public int increment;
+        public int timeout;
+        public int[] ports;
+        string ip;
+        bool openonly;
+
+        public ScanSeg(string ipaddr, int[] portA, int start, int inc, int timeo, bool openon)
+        {
+            this.openonly = openon;
+            this.ip = ipaddr;
+            this.ports = portA;
+            this.starti = start;
+            this.timeout = timeo;
+            this.increment = inc;
+        }
+
+        public void run()
+        {
+            for (int i = starti; i < ports.Length; i += increment)
+                trySocket(ip, ports[i], timeout);
+        }
+
+        public void trySocket(string ip, int port, int timeout)
+	    {
+		    //Create socket connection
+		    try {
+			    //TcpClient client = new TcpClient(ip, port);
+                using (TcpClient tcp = new TcpClient())
+                {
+                    IAsyncResult ar = tcp.BeginConnect(ip, port, null, null);
+                    System.Threading.WaitHandle wh = ar.AsyncWaitHandle;
+                    try
+                    {
+                        if (!ar.AsyncWaitHandle.WaitOne(timeout, false))
+                        {
+                            tcp.Close();
+                            throw new TimeoutException();
+                        }
+
+                        tcp.EndConnect(ar);
+                    }
+                    finally
+                    {
+                        wh.Close();
+                    }
+                }
+                Console.Out.WriteLine(ip+"\t"+port+"\tS");
+		    } catch (Exception e) {
+			    if (!openonly)
+				    Console.Out.WriteLine(ip+"\t"+port+"\tF\t"+e.Message);
+		    }
+	    }
+    };
+
+    //main
     class Program
     {
         static int[] ports;
+        static ArrayList threadList;
 
         static void Main(string[] args)
         {
+            threadList = new ArrayList();
+
             ports = new int[45];
             ports[0] = 7;
             ports[1] = 18;
@@ -57,7 +122,7 @@ namespace cs_scan
             ports[43] = 569;
             ports[44] = 1080;
 
-
+            
             string ip = args[0];
             int timeout = Convert.ToInt32(args[1]);
             int threads = Convert.ToInt32(args[2]);
@@ -77,8 +142,11 @@ namespace cs_scan
                             string ips = ip + k + "." + j;
                             for (int i = 0; i < threads; i++)
                             {
-                                //threadSleep(threads, timeout);
-                                //(new Thread(new ScanSeg(ips, ports, i, threads, timeout, openonly))).start();
+                                threadSleep(threads, timeout);
+                                ScanSeg s = new ScanSeg(ips, ports, i, threads, timeout, openonly);
+                                Thread thread = new Thread(() => s.run());
+                                threadList.Add(thread);
+                                thread.Start();
                             }
                         }
                     }
@@ -90,8 +158,11 @@ namespace cs_scan
                         string ips = ip + j;
                         for (int i = 0; i < threads; i++)
                         {
-                            //threadSleep(threads, timeout);
-                            //(new Thread(new ScanSeg(ips, ports, i, threads, timeout, openonly))).start();
+                            threadSleep(threads, timeout);
+                            ScanSeg s = new ScanSeg(ips, ports, i, threads, timeout, openonly);
+                            Thread thread = new Thread(() => s.run());
+                            threadList.Add(thread);
+                            thread.Start();
                         }
                     }
                 }
@@ -99,10 +170,28 @@ namespace cs_scan
             else
                 for (int i = 0; i < threads; i++)
                 {
-                    //(new Thread(new ScanSeg(ip, ports, i, threads, timeout, openonly))).start();
+                    ScanSeg s = new ScanSeg(ip, ports, i, threads, timeout, openonly);
+                    Thread thread = new Thread(() => s.run());
+                    threadList.Add(thread);
+                    thread.Start();
                 }
-		
-            Console.In.ReadLine();
+        }
+
+
+
+        static void threadSleep(int threads, int timeout)
+        {
+            while (threadList.Count >= threads)
+            {
+                for (int i = threadList.Count - 1; i >= 0; i--)
+                {
+                    Thread t = (Thread)threadList[i];
+                    if (t.ThreadState == System.Threading.ThreadState.Stopped)
+                        threadList.Remove(t);
+                }
+
+                Thread.Sleep(timeout / 10);
+            }
         }
     }
 }
